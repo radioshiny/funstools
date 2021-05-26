bugfix about masking and smoothing method

funstools/cube2map.py
@@ -156,7 +156,12 @@ class Cube2map:
             3d-array (cube) filled with 0. or 1.
         """
         if self._mask is None:
-            self._mask = get_mask(self._data, self._snr, self.rms, self._maxrms, self._velsmo, verbose=True)
+            # self._mask = get_mask(self._data, self._snr, self.rms, self._maxrms, self._velsmo, verbose=True)
+            if self._smoothing:
+                maxrms = self._maxrms*np.nanmedian(self.srms)/np.nanmedian(self.rms)
+                self._mask = get_mask(self.sdata, self._snr, self.srms, maxrms, verbose=True)
+            else:
+                self._mask = get_mask(self._data, self._snr, self.rms, self._maxrms, verbose=True)
         return self._mask
 
     @property
@@ -214,7 +219,7 @@ class Cube2map:
         Data cube with masking and smoothing.
         """
         if self._smdata is None:
-            self._smdata = smooth3d(self.mdata, self._spasmo, self._velsmo)
+            self._smdata = self.mask*self.sdata
         return self._smdata
 
     @property
@@ -235,7 +240,10 @@ class Cube2map:
                 else:
                     self._y = self.sdata
             else:
-                self._y = self.mdata
+                if self._masking:
+                    self._y = self.mdata
+                else:
+                    self._y = self._data
         return self._y
 
     @property

funstools/makemaps.py
@@ -46,7 +46,8 @@ def get_rms(data, where='both', size=None, ext=None):
         raise ValueError("'{}' is not recognized. Possible options: 'left', 'right', or 'both'.".format(where))
 
 
-def get_mask(data, snr=3., rms=None, max_rms=None, velocity_smo=1, ext=None, ext_rms=None, verbose=True):
+# def get_mask(data, snr=3., rms=None, max_rms=None, velocity_smo=1, ext=None, ext_rms=None, verbose=True):
+def get_mask(data, snr=3., rms=None, max_rms=None, ext=None, ext_rms=None, verbose=True):
     """
     Check the detection of emission lines for each channel in the cube data
     and return a mask that makes the undetected channels zero.
@@ -62,8 +63,8 @@ def get_mask(data, snr=3., rms=None, max_rms=None, velocity_smo=1, ext=None, ext
         max_rms : float
             Maximum limit of rms. Pixels with worse rms are excluded.
             Default is '2 x snr x median(rms)'
-        velocity_smo : float
-            Channel smoothing size for detection.
+        # velocity_smo : float
+        #     Channel smoothing size for detection.
         ext : int, optional
             Extension number when input FITS is multiple HDUList.
         ext_rms : int, optional
@@ -93,7 +94,8 @@ def get_mask(data, snr=3., rms=None, max_rms=None, velocity_smo=1, ext=None, ext
     else:
         max_rms = float(max_rms)
     mask = np.zeros_like(temp)
-    smodata = smooth1d(temp, velocity_smo)
+    # smodata = smooth1d(temp, velocity_smo)
+    # remove smoothing in make_mask, just use input data
     empty, noisy = 0, 0
     for d in range(rms.shape[0]):
         for r in range(rms.shape[1]):
@@ -105,14 +107,15 @@ def get_mask(data, snr=3., rms=None, max_rms=None, velocity_smo=1, ext=None, ext
                 noisy += 1
                 mask[:, d, r] = np.nan
                 continue
-            y = smodata[:, d, r]
+            # y = smodata[:, d, r]
+            y = temp[:, d, r]
             pc = np.where(y > 0.)[0]
             for k, g in groupby(enumerate(pc), lambda x: x[0]-x[1]):
                 g = list(map(itemgetter(1), g))
                 if len(g) > 3 and any(y[g] > snr*rms[d, r]):
                     mask[g, d, r] = 1.
     det = np.nansum(mask, axis=0) > 4.
-    mask[:, ~det] = np.nan
+    # mask[:, ~det] = np.nan
     if verbose:
         print('\n[ Masking noise channels ]')
         print('Total pixels        = {:d}'.format(np.prod(rms.shape)))

funstools/smooth.py
@@ -3,6 +3,8 @@ from astropy.convolution.kernels import Gaussian1DKernel, Gaussian2DKernel, Toph
 from astropy import units as u
 import numpy as np
 
+_f2s = np.sqrt(8.*np.log(2.))
+
 
 def smooth1d(data, fwhm):
     """
@@ -19,7 +21,7 @@ def smooth1d(data, fwhm):
     """
     if float(fwhm) == 0.:
         return data
-    kernel = Gaussian1DKernel(fwhm/np.sqrt(8.*np.log(2.)))
+    kernel = Gaussian1DKernel(fwhm/_f2s)
     kl = len(kernel.array)
     data = np.r_[data[kl-1:0:-1], data, data[-2:-kl-1:-1]]
     if len(data.shape) == 1:
@@ -53,13 +55,13 @@ def smooth2d(data, fwhm, pa=None):
     if float(fwhm) == 0.:
         return data
     if isinstance(fwhm, int) or isinstance(fwhm, float):
-        kernel = Gaussian2DKernel(fwhm/np.sqrt(8.*np.log(2.)))
+        kernel = Gaussian2DKernel(fwhm/_f2s)
     elif len(fwhm) == 2:
         if pa is None:
-            kernel = Gaussian2DKernel(fwhm[0]/np.sqrt(8.*np.log(2.)), fwhm[1]/np.sqrt(8.*np.log(2.)))
+            kernel = Gaussian2DKernel(fwhm[0]/_f2s, fwhm[1]/_f2s)
         else:
             theta = pa*u.deg.to(u.rad)
-            kernel = Gaussian2DKernel(fwhm[0]/np.sqrt(8.*np.log(2.)), fwhm[1]/np.sqrt(8.*np.log(2.)), theta)
+            kernel = Gaussian2DKernel(fwhm[0]/_f2s, fwhm[1]/_f2s, theta)
     else:
         raise ValueError('2D-smoothing size: float (fwhm) or tuple (x_fwhm, y_fwhm)')
     if len(data.shape) == 1: